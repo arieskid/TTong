ad076e9cf0059fca6510b16b4c36f04f474bfc80
arieskid <mnih44@gmail.com>
2015-09-24 21:33:03 +0900

    login OK

--- TTong/src/com/example/activity/LoginActivity.java
+++ TTong/src/com/example/activity/LoginActivity.java
@@ -1,48 +1,119 @@
 package com.example.activity;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import com.example.ttong.R;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends Activity implements OnClickListener{
+public class LoginActivity extends Activity {
 
+	private final String SERVER_ADDRESS = "http://14.63.226.208";
 	EditText nameET, phoneET;
 	Button btn;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
+		StrictMode.enableDefaults();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		
 		nameET = (EditText) findViewById(R.id.name);
 		phoneET = (EditText) findViewById(R.id.phone);
 		btn = (Button) findViewById(R.id.okBtn);
-		btn.setOnClickListener(this);
-	}
+		btn.setOnClickListener(new View.OnClickListener() { // 입력 버튼을 눌렀을 때
 
-	@Override
-	public void onClick(View v) {
-	
-		SharedPreferences preference = MainActivity.pref;
-		SharedPreferences.Editor editor = preference.edit();
-		
-		String name = nameET.getText().toString();
-		String phone = phoneET.getText().toString();
-		
-		editor.putBoolean("Status", true);
-		editor.putString("UserName", name);
-		editor.putString("UserPhone", phone);
-		editor.commit();
-		
-		Intent i = new Intent(this, MainActivity.class);
-		startActivity(i);
+			public void onClick(View v) {				
+				// TODO Auto-generated method stub
+				if (nameET.getText().toString().equals("") || phoneET.getText().toString().equals("")) {					
+					Toast.makeText(LoginActivity.this, "이름과 전화번호를 입력하세요!", Toast.LENGTH_SHORT).show();
+					return;
+				}
+				
+				runOnUiThread(new Runnable() {
+
+					public void run() {
+						SharedPreferences preference = MainActivity.pref;
+						SharedPreferences.Editor editor = preference.edit();
+						
+						// TODO Auto-generated method stub
+						String name = nameET.getText().toString();
+						String phone = phoneET.getText().toString();
+
+						try {
+							URL url = new URL(
+									SERVER_ADDRESS + "/ttong_insert.php?" + "name=" + URLEncoder.encode(name, "UTF-8")
+											+ "&phone=" + URLEncoder.encode(phone, "UTF-8"));
+							url.openStream();
+							
+							String result = getXmlData("insertresult.xml", "result"); 
+							
+							if (result.equals("1")) {
+								Toast.makeText(LoginActivity.this, "가입을 축하합니다.", Toast.LENGTH_SHORT).show();
+								
+								nameET.setText("");
+								phoneET.setText("");
+								
+								editor.putBoolean("Status", true);
+								editor.putString("UserName", name);
+								editor.putString("UserPhone", phone);
+								editor.commit();
+								
+								Intent i = new Intent(LoginActivity.this, MainActivity.class);
+								startActivity(i);
+							} else 
+								Toast.makeText(LoginActivity.this, "가입에 실패했습니다. 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
 	}
 	
+	private String getXmlData(String filename, String str) {
+        String rss = SERVER_ADDRESS + "/result/";
+        String ret = "";
+         
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            XmlPullParser xpp = factory.newPullParser();
+            URL server = new URL(rss + filename);
+            InputStream is = server.openStream();
+            xpp.setInput(is, "UTF-8");
+             
+            int eventType = xpp.getEventType();
+             
+            while(eventType != XmlPullParser.END_DOCUMENT) {
+                if(eventType == XmlPullParser.START_TAG) {
+                    if(xpp.getName().equals(str)) {
+                        ret = xpp.nextText();
+                    }
+                }
+                eventType = xpp.next();
+            }
+        } catch(Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+         
+        return ret;
+    }	
 }
--- TTong/src/com/example/activity/MainActivity.java
+++ TTong/src/com/example/activity/MainActivity.java
@@ -1,29 +1,34 @@
 package com.example.activity;
 
-
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import com.example.ttong.R;
 
-import Adapter.MainListViewAdapter;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends Activity implements OnItemClickListener{
-    
-	ListView listView;
-	MainListViewAdapter adapter;
+public class MainActivity extends Activity{
+	private final String SERVER_ADDRESS = "http://14.63.226.208";
+	
+	ListView list;
+	ArrayList<String> data;
+    ArrayAdapter<String> adapter;
 	
 	public static SharedPreferences pref; 
 	SharedPreferences.Editor editor;
-	ArrayList<String> arr;
+	
 	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,40 +37,84 @@ public class MainActivity extends Activity implements OnItemClickListener{
         pref = getSharedPreferences("TTongLogin", 0);
         editor = pref.edit();
         
-        listView = (ListView) findViewById(R.id.listView);
-        adapter = new MainListViewAdapter(this, null);
-        listView.setOnItemClickListener(this);
-        listView.setAdapter(adapter);
+        list = (ListView) findViewById(R.id.listView);
+        data = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
         
         checkLogin();
-    }
+    }	
 
     public void checkLogin(){
-    	if(!pref.getBoolean("Status", false)){	// true=login, false=not login 
+    	if(!pref.getBoolean("Status", false)) {	// true=login, false=not login 
     		Intent i = new Intent(MainActivity.this, LoginActivity.class);
     		startActivity(i);
-    	}else{
-    		setData();
+    	} else {
+    		final Handler handler = new Handler();
+            runOnUiThread(new Runnable() {
+                 
+                public void run() {
+                    // TODO Auto-generated method stub
+                    final ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "불러오는중.....", "잠시만 기다려주세요.");
+                     
+                    handler.post(new Runnable() {
+                         
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            try {
+                                data.clear(); //반복적으로 누를경우 똑같은 값이 나오는 것을 방지하기 위해 data를 클리어함
+                                URL url = new URL(SERVER_ADDRESS + "/ttong_search.php");
+                                url.openStream(); //서버의 serarch.php파일을 실행함
+                     
+                                ArrayList<String> namelist = getXmlDataList("searchresult.xml", "name");//name 태그값을 읽어 namelist 리스트에 저장
+                                ArrayList<String> phone_number_list = getXmlDataList("searchresult.xml", "phone_number"); //price 태그값을 읽어 prica 리스트에 저장
+                                if(namelist.isEmpty()) {
+                                    data.add(new String("아무것도 검색되지 않았습니다."));
+                                } else {
+                                    for(int i = 0; i < namelist.size(); i++) {
+                                        String str = namelist.get(i) + " - " + phone_number_list.get(i);
+                                        data.add(str);
+                                    }
+                                }
+                            } catch(Exception e) {
+                               // Log.e("Error", e.getMessage());
+                            } finally{
+                                dialog.dismiss();
+                                adapter.notifyDataSetChanged();
+                            }
+                        }
+                    });
+                }
+            });
     	}
     }
-    
-    public void setData(){
-    	arr = new ArrayList<String>();
-    	arr.add("professor");
-    	arr.add("in");
-    	arr.add("chancejin");
-    	arr.add("dahyeon");
-    	
-    	adapter.setData(arr);
-    	adapter.notifyDataSetChanged();
-    }
 
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		Intent intent = new Intent(this, CallActivity.class);
-		String str = adapter.getData().get(position);
-		intent.putExtra("name", str);
-		startActivity(intent);
+	private ArrayList<String> getXmlDataList(String filename, String str) { // 태그값 여러 개를 받아오기 위한 ArrayList<string>형 변수
+		String rss = SERVER_ADDRESS + "/result/";
+		ArrayList<String> ret = new ArrayList<String>();
+
+		try { // XML 파싱을 위한 과정
+			XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+			factory.setNamespaceAware(true);
+			XmlPullParser xpp = factory.newPullParser();
+			URL server = new URL(rss + filename);
+			InputStream is = server.openStream();
+			xpp.setInput(is, "UTF-8");
+
+			int eventType = xpp.getEventType();
+
+			while (eventType != XmlPullParser.END_DOCUMENT) {
+				if (eventType == XmlPullParser.START_TAG) {
+					if (xpp.getName().equals(str)) { // 태그 이름이 str 인자값과 같은 경우
+						ret.add(xpp.nextText());
+					}
+				}
+				eventType = xpp.next();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ret;
 	}
-    
 }
