4a90fee715e3517fd558f0e0d2c6ba54c80083a4
DAHYEON-PARK <luckyclover0121@gmail.com>
2015-09-27 16:37:10 +0900

    add refresh func

--- TTong/src/com/ttong/activity/MainActivity.java
+++ TTong/src/com/ttong/activity/MainActivity.java
@@ -99,6 +99,50 @@ public class MainActivity extends Activity {
 	}
     
     
+	public void getDataFromDB(){
+		final Handler handler = new Handler();
+        runOnUiThread(new Runnable() {
+             
+            public void run() {
+                // TODO Auto-generated method stub
+                final ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "불러오는중.....", "잠시만 기다려주세요.");
+                 
+                handler.post(new Runnable() {
+                     
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        try {
+                            data.clear(); //반복적으로 누를경우 똑같은 값이 나오는 것을 방지하기 위해 data를 클리어함
+                            URL url = new URL(SERVER_ADDRESS + "/ttong_search.php");
+                            url.openStream(); //서버의 serarch.php파일을 실행함
+                 
+                            ArrayList<String> namelist = getXmlDataList("searchresult.xml", "name");//name 태그값을 읽어 namelist 리스트에 저장
+                            ArrayList<String> phone_number_list = getXmlDataList("searchresult.xml", "phone_number"); //price 태그값을 읽어 prica 리스트에 저장
+                            if(namelist.isEmpty()) {
+                            	UserData ud = new UserData("default","000-0000-0000",0);
+                            	data.add(ud);
+                            } else {
+                                for(int i = 0; i < namelist.size(); i++) {
+                                	////////////////////////////////////////////////////////
+                                	UserData ud = new UserData(namelist.get(i), phone_number_list.get(i), 0);
+                                    data.add(ud);
+                                }
+                            }
+                        } catch(Exception e) {
+                           // Log.e("Error", e.getMessage());
+                        } finally{
+                            dialog.dismiss();
+                            
+                            connect(); // 통신 서버
+                            
+                            adapter.setData(data);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+            }
+        });
+	}
     
     public void checkLogin(){
     	
@@ -106,48 +150,7 @@ public class MainActivity extends Activity {
     		Intent i = new Intent(MainActivity.this, RegisterActivity.class);
     		startActivity(i);
     	} else {
-    		final Handler handler = new Handler();
-            runOnUiThread(new Runnable() {
-                 
-                public void run() {
-                    // TODO Auto-generated method stub
-                    final ProgressDialog dialog = ProgressDialog.show(MainActivity.this, "불러오는중.....", "잠시만 기다려주세요.");
-                     
-                    handler.post(new Runnable() {
-                         
-                        public void run() {
-                            // TODO Auto-generated method stub
-                            try {
-                                data.clear(); //반복적으로 누를경우 똑같은 값이 나오는 것을 방지하기 위해 data를 클리어함
-                                URL url = new URL(SERVER_ADDRESS + "/ttong_search.php");
-                                url.openStream(); //서버의 serarch.php파일을 실행함
-                     
-                                ArrayList<String> namelist = getXmlDataList("searchresult.xml", "name");//name 태그값을 읽어 namelist 리스트에 저장
-                                ArrayList<String> phone_number_list = getXmlDataList("searchresult.xml", "phone_number"); //price 태그값을 읽어 prica 리스트에 저장
-                                if(namelist.isEmpty()) {
-                                	UserData ud = new UserData("default","000-0000-0000",0);
-                                	data.add(ud);
-                                } else {
-                                    for(int i = 0; i < namelist.size(); i++) {
-                                    	////////////////////////////////////////////////////////
-                                    	UserData ud = new UserData(namelist.get(i), phone_number_list.get(i), 0);
-                                        data.add(ud);
-                                    }
-                                }
-                            } catch(Exception e) {
-                               // Log.e("Error", e.getMessage());
-                            } finally{
-                                dialog.dismiss();
-                                
-                                connect(); // 통신 서버
-                                
-                                adapter.setData(data);
-                                adapter.notifyDataSetChanged();
-                            }
-                        }
-                    });
-                }
-            });
+    		getDataFromDB();
     	}
     }
 
@@ -166,11 +169,14 @@ public class MainActivity extends Activity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
-		
 			Intent i = new Intent(this, SettingActivity.class);
 			i.putExtra("userName", pref.getString("UserName", "no name"));
 			i.putExtra("userPhone", pref.getString("UserPhone", "no phone number"));
 			startActivity(i);
+			return true;
+		}
+		else if (id == R.id.action_refresh) {
+			getDataFromDB();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
